19fa1e1524caa81668046876b2fbbe6f3ac706fd
lyubomyr <lyubomyr.fedevitch@gmail.com>
2022-07-06 20:02:51 +0300

    amendment

--- homelib-scanner/src/fileScanner/cbProcessor.ts
+++ homelib-scanner/src/fileScanner/cbProcessor.ts
@@ -5,7 +5,7 @@ const comic = require('comic-info');
 
 const parseComicBook = async (fileName: string): Promise<ProcessResult> => {
     const _meta = await comic({path: fileName});
-    return { rawText: '', meta: _.omit(_meta, 'dir'), pages: _.get(_meta, 'pages', 0) };          
+    return { rawText: '', meta: _.omit(_meta, 'dir'), pages: Number.parseInt(_.get(_meta, 'pages', '0')) };          
 }
 
 const processComicBook = async (fileName: string): Promise<ProcessResult> => {

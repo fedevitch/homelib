41035dfcdb1331c2dab43db2a6333d031f86836e
lyubomyr <lyubomyr.fedevitch@gmail.com>
2022-07-07 12:51:07 +0300

    comicbook parser fix

--- homelib-scanner/build/fileScanner/cbProcessor.js
+++ homelib-scanner/build/fileScanner/cbProcessor.js
@@ -7,7 +7,7 @@ const lodash_1 = __importDefault(require("lodash"));
 const logger_1 = __importDefault(require("../logger"));
 const comic = require('comic-info');
 const parseComicBook = async (fileName) => {
-    const _meta = await comic({ path: fileName });
+    const _meta = await new comic({ path: fileName });
     return { rawText: '', meta: lodash_1.default.omit(_meta, 'dir'), pages: Number.parseInt(lodash_1.default.get(_meta, 'pages', '0')) };
 };
 const processComicBook = async (fileName) => {
--- homelib-scanner/src/fileScanner/cbProcessor.ts
+++ homelib-scanner/src/fileScanner/cbProcessor.ts
@@ -4,7 +4,7 @@ import logger from "../logger";
 const comic = require('comic-info');
 
 const parseComicBook = async (fileName: string): Promise<ProcessResult> => {
-    const _meta = await comic({path: fileName});
+    const _meta = await new comic({path: fileName});
     return { rawText: '', meta: _.omit(_meta, 'dir'), pages: Number.parseInt(_.get(_meta, 'pages', '0')) };          
 }
 

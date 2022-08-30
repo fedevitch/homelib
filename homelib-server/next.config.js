d1359afd2e1c580f9047a7488f037bc8240d8e79
lyubomyr <lyubomyr.fedevitch@gmail.com>
2022-08-30 13:08:42 +0300

    turnet on eslint

--- homelib-server/next.config.js
+++ homelib-server/next.config.js
@@ -11,12 +11,7 @@ const nextConfig = {
   },
   api: {
     responseLimit: false,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  }
 }
 
 module.exports = nextConfig
